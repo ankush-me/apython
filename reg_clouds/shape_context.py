b00886303e055c65291e90f602cd77a873757aea
Ankush Gupta <ankush.me@hotmail.com>
2013-06-24 20:49:02 -0700

    added shape_context calculation

--- reg_clouds/shape_context.py
+++ reg_clouds/shape_context.py
@@ -5,66 +5,56 @@ from mayavi import mlab
 from rapprentice.registration import loglinspace
 
 
-def shape_context(p, median_dist=None, r_inner=1./8, r_outer=2, nbins_r=5, nbins_theta=12, nbins_phi=5, outliers=None):
+def shape_context(p, median_dist=None, r_inner=1./8, r_outer=2, nbins_r=5, nbins_theta=12, nbins_phi=6, outliers=None):
     """
     Computes the shape-context log-polar histograms at each point in p -- the point cloud.
     
     p is a Nxd matrix of points.
     """
-    N, d = p.shape
     
+    N, d = p.shape
     assert d==3, "shape_context is implemented only for three dimensions"
     
     p_mean = np.mean(p, axis=0)
     p_centered = p - p_mean
-    _,_,v      = np.linalg.svd(p_centered, full_matrices=1)
+    _,_,v      = np.linalg.svd(p_centered, full_matrices=True)
     pt_nd      = np.dot(p_centered, v.T)
     
-
-    dx_nn, dy_nn, dz_nn  = pt_nd.T[:,:,None]-pt_nd.T[:,None,:]
-    
-    dists    = ssd.cdist(pt_nd, 'euclidean')
+    # compute the coordinates : r,theta, phi
+    dists    = ssd.pdist(pt_nd, 'euclidean')
     if median_dist==None:
         median_dist = np.median(dists)
         dists       = dists/median_dist
-
-    # compute bin-membership for lengths
-    r_bin_edges  = np.concatenate(([-1], loglinspace(r_inner, r_outer, nbins_r)))
-    r_bin_id_nn  = ssd.squareform(np.digitize(dists, bins=r_bin_edges))
-    fz_nn = r_bin_id_nn < nbins_r # flag all points inside outer boundary
-
+    dists_nn        = ssd.squareform(dists)
 
     # theta_nn are in [0,2pi)
-    theta_nn = np.arctan2(dy_nn, dx_nn)
-    theta_nn = np.mod(np.mod(theta_nn,2*np.pi)+2*np.pi,2*np.pi)
-
+    dx_nn, dy_nn, dz_nn  = pt_nd.T[:,:,None]-pt_nd.T[:,None,:]
+    theta_nn        = np.arctan2(dy_nn, dx_nn)
+    theta_nn        = np.mod(np.mod(theta_nn,2*np.pi)+2*np.pi,2*np.pi)
 
     # phi_nn are in [-pi/2, pi/2]
-    dist_xy_nn  = (dx_nn**2 + dy_nn**2)**0.5
-    phi_nn = np.arctan2(dz_nn, dist_xy_nn)
+    dist_xy_nn    = (dx_nn**2 + dy_nn**2)**0.5
+    phi_nn        = np.arctan2(dz_nn, dist_xy_nn)
+
+    # define histogram edges
+    r_edges     = np.concatenate(([0], loglinspace(r_inner, r_outer, nbins_r)))
+    theta_edges = np.linspace(0, 2*np.pi, nbins_theta+1)
+    phi_edges   = np.linspace(-np.pi/2, np.pi/2, nbins_phi+1) 
 
 
+    combined_3nn = np.array([dists_nn, theta_nn, phi_nn])
     
+    # compute the bins : 4 dimensional matrix.
+    # r,t,p are the number of bins of radius, theta, phi
+    sc_nrtp = np.zeros((N, nbins_r, nbins_theta, nbins_phi))
     
-    nbins  = nbins_r * nbins_theta * nbins_phi
-    sc     = np.zeros(N,nbins) 
-    
+    for i in xrange(N):
+        hist, edges = np.histogramdd(combined_3nn[:,i,:].T, bins=[r_edges, theta_edges, phi_edges])
+        sc_nrtp[i,:,:,:] = hist 
 
-    # quantize to a fixed set of angles (bin edges lie on 0,(2*pi)/k,...2*pi
-    theta_array_q = 1+floor(theta_array_2/(2*pi/nbins_theta));
+    return sc_nrtp 
 
-    nbins=nbins_theta*nbins_r;
-    BH=zeros(nsamp,nbins);
-    for n=1:nsamp
-        fzn=fz(n,:)&in_vec;
-        Sn=sparse(theta_array_q(n,fzn),r_array_q(n,fzn),1,nbins_theta,nbins_r);
-        BH(n,:)=Sn(:)';
 
-    
-    
-    
-    
-        
 
 if __name__=='__main__':
     N = 100
@@ -78,6 +68,3 @@ if __name__=='__main__':
 
     mlab.points3d(p[:,0], p[:,1], p[:,2], scale_factor=0.1)
     shape_context(p)
-    
-    
-    
